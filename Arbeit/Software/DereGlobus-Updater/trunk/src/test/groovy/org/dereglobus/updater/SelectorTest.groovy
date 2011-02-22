b5aa83771a2a3ea99de7c2feeccb45578dc831d0
marc.vonrenteln@googlemail.com <marc.vonrenteln@googlemail.com@33a08ba3-6f65-ab0b-70f3-e24c380a5552>
2011-02-22 14:27:56 +0000

    Tests auf geänderte Klassen umgestellt

---
+++ Arbeit/Software/DereGlobus-Updater/trunk/src/test/groovy/org/dereglobus/updater/SelectorTest.groovy
@@ -0,0 +1,53 @@
+package org.dereglobus.updater
+
+import org.dereglobus.updater.tree.FileSystemCheckModel;
+
+import junit.framework.TestCase
+
+class SelectorTest extends TestCase {
+
+	public void testCopy() {
+		def basePath = new File(new File("").getAbsolutePath())
+		println "base path: "+basePath.absolutePath
+		assert basePath.exists() && basePath.isDirectory()
+		def model = new FileSystemCheckModel(basePath)
+		def childs = model.getChilds(model.getRoot())
+		assert 3 == childs.size()
+		model.getChilds(childs[1])
+
+		def child = model.getChild(model.getRoot(), 1)
+		assert "src" == child.userObject.getName()
+		child.setSelected(true)
+		assert child.isSelected()
+		child.children().nextElement().setSelected(true)
+
+		assert 2 == new Selector(new Config()).getFolders(model.getRoot()).size()
+	}
+
+	public void testCopyUnselectedParent() {
+		def basePath = new File(new File("").getAbsolutePath())
+		println "base path: "+basePath.absolutePath
+		assert basePath.exists() && basePath.isDirectory()
+		def model = new FileSystemCheckModel(basePath)
+		def childs = model.getChilds(model.getRoot())
+		model.getChilds(childs[1])
+
+		def child = model.getChild(model.getRoot(), 1)
+		assert "src" == child.userObject.getName()
+		child.children().nextElement().setSelected(true)
+
+		assert 1 == new Selector(new Config()).getFolders(model.getRoot()).size()
+	}
+
+	public void testCopyRoot() {
+		def basePath = new File(new File("").getAbsolutePath())
+		println "base path: "+basePath.absolutePath
+		assert basePath.exists() && basePath.isDirectory()
+		def model = new FileSystemCheckModel(basePath)
+		def childs = model.getChilds(model.getRoot())
+
+		model.getRoot().setSelected(true)
+
+		assert 1 == new Selector(new Config()).getFolders(model.getRoot()).size()
+	}
+}

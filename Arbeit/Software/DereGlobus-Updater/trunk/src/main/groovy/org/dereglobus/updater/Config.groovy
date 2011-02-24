07937fba7f953a788d4896ab0935f914ffc28205
marc.vonrenteln <marc.vonrenteln@googlemail.com>
2011-02-24 08:04:42 +0000

    Konfigurations-Möglichkeiten für ftpServer, ftpPath, releaseUrl und publicUrl

--- Arbeit/Software/DereGlobus-Updater/trunk/src/main/groovy/org/dereglobus/updater/Config.groovy
+++ Arbeit/Software/DereGlobus-Updater/trunk/src/main/groovy/org/dereglobus/updater/Config.groovy
@@ -10,21 +10,17 @@ class Config {
 	static final String DEST_PATH = "DEST_PATH"
 	static final String FTP_USER = "FTP_USER"
 	static final String FTP_PASS = "FTP_PASS"
+	static final String FTP_SERVER = "FTP_SERVER"
+	static final String FTP_PATH = "FTP_PATH"
+	static final String RELEASE_URL = "RELEASE_URL"
+	static final String PUBLIC_URL = "PUBLIC_URL"
 
 	Preferences prefs
 
 	def log = new StringBuffer()
 
-	Filter filter
-
-	String ftpServer = "dereglobus.orkenspalter.com"
-
-	//	String ftpPath = "dereglobus.orkenspalter.de/public/DereGlobus"
-	String ftpPath = "dereglobus.orkenspalter.de/public/DereGlobusTest"
-
 	public Config() {
 		prefs = Preferences.userRoot().node(nodeName)
-		filter = new SimpleServerUrlFilter("http://www.dereglobus.orkenspalter.com/svn/Release/", "http://www.dereglobus.orkenspalter.de/public/")
 	}
 
 	String getSourcePath() {
@@ -57,5 +53,41 @@ class Config {
 
 	void setFtpPass(String pass) {
 		prefs.put(FTP_PASS, pass)
+	}
+
+	String getFtpServer() {
+		return prefs.get(FTP_SERVER, "dereglobus.orkenspalter.com")
+	}
+
+	void setFtpServer(String server) {
+		prefs.put(FTP_SERVER, server)
+	}
+
+	String getFtpPath() {
+		return prefs.get(FTP_PATH, "dereglobus.orkenspalter.de/public/DereGlobusTest")
+	}
+
+	void setFtpPath(String path) {
+		prefs.put(FTP_PATH, path)
+	}
+
+	String getReleaseUrl() {
+		return prefs.get(RELEASE_URL, "http://www.dereglobus.orkenspalter.com/svn/Release/")
+	}
+
+	void setReleaseUrl(String path) {
+		prefs.put(RELEASE_URL, path)
+	}
+
+	String getPublicUrl() {
+		return prefs.get(PUBLIC_URL, "http://www.dereglobus.orkenspalter.de/public/")
+	}
+
+	void setPublicUrl(String path) {
+		prefs.put(PUBLIC_URL, path)
+	}
+
+	Filter getFilter() {
+		return new SimpleServerUrlFilter(getReleaseUrl(), getPublicUrl())
 	}
 }
